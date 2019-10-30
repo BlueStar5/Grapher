bc367d3e8de8f86005607f047d672f7f7e093713
BlueStar5 <oobluestar5oo@gmail.com>
2019-10-30 00:15:17 -0400

    Replace old Transformation code with new

--- src/events/Transformation.ts
+++ src/events/Transformation.ts
@@ -1,20 +1,4 @@
 abstract class Transformation {
-  id = 0;
-  name: string;
-  object: GeomObject;
-  preImage: GeomObject;
-  image: GeomObject;
-  exclude: GeomObject[];
-  constructor(object: GeomObject, args?: {
-    exclude?: GeomObject[];
-    preImage?: GeomObject;
-    image?: GeomObject;
-  }) {
-    this.object = object;
-    if (args) {
-      this.exclude = args.exclude;
-      this.preImage = args.preImage;
-      this.image = args.image;
-    }
-  }
+  id: number = 0;
+  abstract apply(obj: GeomObject): GeomObject;
 }
--- src/events/Translation.ts
+++ src/events/Translation.ts
@@ -1,42 +1,10 @@
 class Translation extends Transformation {
   vector: Vector;
-  constructor(object: GeomObject, vector: Vector, args?: {
-    exclude?: GeomObject[];
-    preImage?: GeomObject;
-    image?: GeomObject;
-  }) {
-    super(object, args);
-    this.name = "translation";
+  constructor(vector: Vector) {
+    super();
     this.vector = vector;
-    if (this.preImage) {
-      this.image = this.preImage.translated(vector);
-    }
-    else if (this.image) {
-      this.preImage = this.image.translated(vector.negative());
-    }
-    else {
-      this.preImage = this.object.translated(vector.negative());
-      this.image = this.object.clone();
-    }
   }
-  getPreImage() {
-    return this.preImage || (this.image ? this.image.translated(this.vector.negative()) : null) || this.object.translated(this.vector.negative());
-  }
-  getImage() {
-    return this.image || (this.preImage ? this.preImage.translated(this.vector) : null) || this.object.clone();
-  }
-  toString() {
-    if (settings.detailedConsole) {
-      return `${this.object.nameString()} ${this.getPreImage().detailsString()} has been translated ${this.vector.detailsString()} to ${this.getImage().detailsString()}.`;
-    }
-    else {
-      return `${this.object.nameString()} has been translated.`;
-    }
-  }
-  equals(transformation) {
-    return this.id === transformation.id && this.name === transformation.name && this.vector.equals(transformation.vector);
-  }
-  similarTo(transformation) {
-    return this.id === transformation.id && this.name === transformation.name;
+  apply(obj: GeomObject): GeomObject {
+    return obj.translated(this.vector);
   }
 }
